bf7ba16e93d00f7138d6f43e08394010fd5a4323
Ad <me@skissue.xyz>
2024-07-29 11:14:10 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "My personal site";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      imports = [inputs.devshell.flakeModule];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devshells.default = with pkgs; {
+          commands = [{package = hugo;}];
+        };
+      };
+    };
+}
